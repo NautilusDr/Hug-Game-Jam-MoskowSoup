1aed61b0d8083a0868b54a53da855db7c475851c
José Carlos <102222057+josecarlos3040@users.noreply.github.com>
2026-03-02 20:05:11 -0300

    animacao do player

--- Assets/Scripts/Player/MovimentacaoTopDownPlayer.cs
+++ Assets/Scripts/Player/MovimentacaoTopDownPlayer.cs
@@ -6,10 +6,13 @@ public class MovimentacaoTopDownPlayer : MonoBehaviour
 {
     InputAction acaoMovimento;
 
+
     [Header("Movimento do Jogador")]
     public float velocidadeBaseJogador;
     public float velocidadeAtualJogador;
     public Rigidbody2D jogadorRigidBody2D;
+    private Animator animator;
+    public SpriteRenderer spriteRenderer;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -18,6 +21,8 @@ public class MovimentacaoTopDownPlayer : MonoBehaviour
         velocidadeAtualJogador = velocidadeBaseJogador;
         jogadorRigidBody2D = GetComponent<Rigidbody2D>();
         acaoMovimento = InputSystem.actions.FindAction("Move");
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -33,10 +38,29 @@ public class MovimentacaoTopDownPlayer : MonoBehaviour
 
     void Movimentacao()
     {
+
         Vector2 moveValue = acaoMovimento.ReadValue<Vector2>();
         Vector2 velocidadeMovimentacao = moveValue * velocidadeAtualJogador;
 
         jogadorRigidBody2D.linearVelocity = velocidadeMovimentacao;
+
+        bool estaMovendo = moveValue != Vector2.zero;
+        animator.SetFloat("moveX", moveValue.x);
+        animator.SetFloat("moveY", moveValue.y);
+        animator.SetBool("isMoving", moveValue != Vector2.zero);
+
+        if(moveValue.x > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+        else if (moveValue.x < 0)
+        {
+            spriteRenderer.flipX= true;
+        }
+        else if (!estaMovendo)
+        {
+            spriteRenderer.flipX = false;
+        }
     }
 
 
