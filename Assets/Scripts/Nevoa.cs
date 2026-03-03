3d97333acc9acde966f60732189cbe467b04e901
Caetano Costa <caetanogonca@gmail.com>
2026-03-03 03:03:43 -0300

    comecei algo

--- Assets/Scripts/Nevoa.cs
+++ Assets/Scripts/Nevoa.cs
@@ -2,15 +2,49 @@ using UnityEngine;
 
 public class Nevoa : MonoBehaviour
 {
-    
+    public bool lupa;
+    public bool lupa2;
+    public bool abrir;
+
     void Start()
     {
-        
+        abrir = false;
+        lupa = false;
+        lupa2 = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if(lupa && lupa2)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Lupa"))
+        {
+            lupa = true;
+        }
+
+        if (collision.gameObject.CompareTag("Lupa2"))
+        {
+            lupa2 = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Lupa"))
+        {
+            lupa = false;
+        }
+
+        if (collision.gameObject.CompareTag("Lupa2"))
+        {
+            lupa2 = false;
+        }
     }
 }
